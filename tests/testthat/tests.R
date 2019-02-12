eb5c477aa653d2baac31df99b7049d8cc225522b
Anne Mickan <a.mickan@donders.ru.nl>
2019-02-12 20:00:43 +0100

    Update tests.R

--- tests/testthat/tests.R
+++ tests/testthat/tests.R
@@ -4,16 +4,16 @@ library(tidyr)
 setwd(system.file("extdata", package = "Fars"))
 
 # load data conventionally
-#data_2014 <- read.csv("accident_2014.csv.bz2")
+data_2014 <- read.csv("accident_2014.csv.bz2")
 # load data with the package's function
-#data_2014_fars <- fars_read("accident_2014.csv.bz2")
+data_2014_fars <- fars_read("accident_2014.csv.bz2")
 
 # compare the output
-#test_that("fars_read output", {
-#  expect_is(data_2014_fars, c("tbl_df", "tbl", "data.frame")) # the output should be a dataframe, tibble or tibble dataframe
-#  expect_equal(colnames(data_2014_fars), colnames(data_2014)) # the column names should be the same as when you read it in with read.csv
-#  expect_error(fars_read("accident_2017.csv.bz2")) # it should throw an error when prompted to read in a file that does not exist
-#})
+test_that("fars_read output", {
+  expect_is(data_2014_fars, c("tbl_df", "tbl", "data.frame")) # the output should be a dataframe, tibble or tibble dataframe
+  expect_equal(colnames(data_2014_fars), colnames(data_2014)) # the column names should be the same as when you read it in with read.csv
+  expect_error(fars_read("accident_2017.csv.bz2")) # it should throw an error when prompted to read in a file that does not exist
+})
 
 # make_filename test
 years <- c(2013, 2014, 2015)
